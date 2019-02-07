7c9ba86bf5accad3400e74eb585ee423874c42e7
Benjamin Grayland <bg3@users.noreply.github.com>
2019-02-07 22:58:29 +1100

    Updated colours

--- pages/other/2_shield_generator/image.js
+++ pages/other/2_shield_generator/image.js
@@ -2,18 +2,17 @@ let daisy = new SVG.Color('#f5f1e6');
 let buttercup = new SVG.Color('#ffcd05');
 let marigold = new SVG.Color('#f19c1f');
 let rose = new SVG.Color('#e13f26');
-let cinnamon = new SVG.Color('#6b3324');
 let forget_me_not = new SVG.Color('#b0e0e3');
+let cinnamon = new SVG.Color('#70422b');
 let hydrangea = new SVG.Color('#3fa0d9');
 let lupine = new SVG.Color('#354C85');
 let sage = new SVG.Color('#899b6d');
 let pine = new SVG.Color('#00503d');
 let azalea = new SVG.Color('#e1a6cb');
-let silver_wattle = new SVG.Color('#8d8b7f');
-//let birch_knot = new SVG.Color('#46443f');
+let silver_wattle = new SVG.Color('#95a4ad');
 let birch_knot = new SVG.Color('#463f46');
-let ironbark = new SVG.Color('#1b1b1b');
-let unknown  = new SVG.Color('#9f7360');
+let ironbark = new SVG.Color('#1c1c1c');
+let unknown  = new SVG.Color('#724f8e');
 let transparent = new SVG.Color('#ff00ff');
 
 const colours = [ daisy, buttercup, marigold, rose, cinnamon, forget_me_not, hydrangea, lupine, sage, pine, azalea, silver_wattle, birch_knot, ironbark, unknown ];
@@ -48,26 +47,45 @@ function Emblem(seed, element, size) {
   let svg = SVG(element).size(size, size);
 
   //  background colour
-  const col_1 = colours[Math.floor(Math.random() * colours.length)];
-  const bg = svg.rect(size, size).attr({ fill: col_1 });
+  c = colours[Math.floor(Math.random() * colours.length)];
+  const bg = svg.rect(size, size).attr({ fill: c });
 
   //  number of elements
   //  next element - cannot be same colour
-  
-  let col_2;
+    
+  let old_col;
 
   do {
-    col_2 = colours[Math.floor(Math.random() * colours.length)];
-  } while (col_2 == col_1);
+    c = colours[Math.floor(Math.random() * colours.length)];
+  } while (c == old_col);
+
+  //  shape functions
+  function three_circles(r1, r2) {
+    //  later extend this to X number of X arranged in a circle.
+    svg.circle(r1, r1).attr({ fill: c }).move(size / 2 - r1 / 2, size / 2 - r1 / 2 - 39);
+    svg.circle(r1, r1).attr({ fill: c }).move(size / 2 - r1 / 2 + 36, size / 2 - r1 / 2 + 25);
+    svg.circle(r1, r1).attr({ fill: c }).move(size / 2 - r1 / 2 - 36, size / 2 - r1 / 2 + 25);
+  }
+
+  function triangle(size, scale, rotation) {
+    //svg.polygon(`0,0 0,${size * scale}, 0,${size * scale}`).fill(c).stroke('none');
+    //svg.polygon('0,0 0,100, 0,100').fill(c).stroke('none');
+    t = svg.polygon(`0,0 0,${ size * scale } ${ size * scale },0`).fill(c);
+    t.transform({ rotation: 90});
+  }
 
   const circleDiameter = size - size / 10;
   const margin = (size - circleDiameter) / 2;
-  svg.circle(circleDiameter, circleDiameter).attr({ fill: col_2 }).move(margin, margin);
+  if (Math.random(1) > 0.5) {
+    triangle(size, 1, 0);
+  } else {
+    svg.circle(circleDiameter, circleDiameter).attr({ fill: c }).move(margin, margin);
+  }
 
   //  next element - can be same colour as either of previous two.
   //  no more than three colours.
 
-  const col_3 = colours[Math.floor(Math.random() * colours.length)];
+  c = colours[Math.floor(Math.random() * colours.length)];
   
   //  draw three circles, arranged in an equilateral triangle.
   //  function to place X items evenly around a circle.
@@ -75,10 +93,12 @@ function Emblem(seed, element, size) {
   //  pass array of items in? pass function to something?
 
   const smCircleDiameter = circleDiameter / 3; 
-  
-  svg.circle(smCircleDiameter, smCircleDiameter).attr({ fill: col_3 }).move(size / 2 - smCircleDiameter / 2, size / 2 - smCircleDiameter / 2 - 39);
-  svg.circle(smCircleDiameter, smCircleDiameter).attr({ fill: col_3 }).move(size / 2 - smCircleDiameter / 2 + 36, size / 2 - smCircleDiameter / 2 + 25);
-  svg.circle(smCircleDiameter, smCircleDiameter).attr({ fill: col_3 }).move(size / 2 - smCircleDiameter / 2 - 36, size / 2 - smCircleDiameter / 2 + 25);
+
+  if (Math.random(1) > 0.5) {
+    three_circles(smCircleDiameter)
+  } else {
+    triangle(size, 0.5, 0);
+  }
 }
 
 const colorpot = new ColourSample(colours, element, size);
