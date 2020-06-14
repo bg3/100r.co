6bb6bc6ae338818894ea8a556cb6156d2f32ce19
Benjamin Grayland <bg3@users.noreply.github.com>
2020-06-14 16:57:40 +1000

    Index page is now sorted by date

--- src/pipi.c
+++ src/pipi.c
@@ -10,13 +10,16 @@
 #include <string.h>
 #include <dirent.h>
 
+/* defining this creates pages that point to ../style.css, as opposed to /style.css */
+//#define LOCAL_BUILD 1;
+
 #define PI_WHITE "#fffff9"
 #define PI_NEWWH "#f7ffff"
 #define PI_BLACK "#111111"
 #define PI_GREY  "#d1d1d1"
 #define PI_RED   "#ef5541"
 
-#define PI_RUNES      " \"+=->#*'|"
+#define PI_RUNES      " \"+=->#*'|/"
 #define PI_PARAGRAPH  ' '
 #define PI_QUOTE      '\"'
 #define PI_IMAGE      '+'
@@ -27,6 +30,7 @@
 #define PI_UNORDERED  '*'
 #define PI_TABLE      '|'
 #define PI_COMMENT    '\''
+#define PI_DATE       '/'
 
 typedef enum { OFF, TABLE, CODE, ORDERED, UNORDERED } block_mode;
 
@@ -34,13 +38,20 @@ typedef enum { OFF, TABLE, CODE, ORDERED, UNORDERED } block_mode;
 
 typedef struct page page;
 typedef struct line line;
+typedef struct date date;
+
+struct date {
+  int year;
+  int month;
+  int day;
+};
 
 struct page {
   char*   title;
   char*   filename;
   char*   section;
-  char*   created;
-  char*   updated;
+  date*   created;
+  date*   updated;
   page*   parent;
   line*   lines;
   int     numlines;
@@ -51,46 +62,6 @@ struct line {
   char* text;
 };
 
-
-/* getline */
-
-char* gline(FILE * f) {
-  size_t size = 0;
-  size_t len  = 0;
-  size_t last = 0;
-  char * buf  = NULL;
-
-  do {
-    size += BUFSIZ; /* BUFSIZ is defined as "the optimal read size for this platform" */
-    buf = realloc(buf,size); /* realloc(NULL,n) is the same as malloc(n) */
-    /* Actually do the read. Note that fgets puts a terminal '\0' on the
-       end of the string, so we make sure we overwrite this */
-    fgets(buf+last,size,f);
-    len = strlen(buf);
-    last = len - 1;
-  } while (!feof(f) && buf[last]!='\n');
-  return buf;
-}
-
-/* append buffer */
-/*
-struct abuf {
-  char *b;
-  int len;
-};
-
-#define ABUF_INIT {NULL, 0}
-
-void abAppend(struct abuf *ab, const char *s, int len) {
-  char *new = realloc(ab->b, ab->len + len);
-  if (new == NULL) return;
-  memcpy(&new[ab->len], s, len);
-  ab->b = new;
-  ab->len += len;
-}
-void abFree(struct abuf *ab) {
-  free(ab->b);
-} */
 
 int string_to_filename(char* str, char* fn) {
   int i = 0;
@@ -119,9 +90,15 @@ int string_to_filename(char* str, char* fn) {
 
 /* html output */
 
-void html_header(FILE* f, char line[]) {
-  fprintf(f, "<!DOCTYPE html>\n <html lang=\"en\">\n <head>\n <title>%s</title>\n <link rel=\"stylesheet\" type=\"text/css\" href=\"/style.css\"> </head>\n <body>\n", line);
-}
+#ifdef LOCAL_BUILD
+  void html_header(FILE* f, char line[]) {
+    fprintf(f, "<!DOCTYPE html>\n <html lang=\"en\">\n <head>\n <title>%s</title>\n <link rel=\"stylesheet\" type=\"text/css\" href=\"../style.css\"> </head>\n <body>\n", line);
+  }
+#else
+  void html_header(FILE* f, char line[]) {
+    fprintf(f, "<!DOCTYPE html>\n <html lang=\"en\">\n <head>\n <title>%s</title>\n <link rel=\"stylesheet\" type=\"text/css\" href=\"/style.css\"> </head>\n <body>\n", line);
+  }
+#endif
 
 void html_banner(FILE* f) {
   fprintf(f, "<header><h2><a href=\"/index.html\">Park</br>Imminent</a></h2></header>");
@@ -243,7 +220,7 @@ void html_unordered_list_end(FILE* f) {
 }
 
 void html_code_start(FILE* f) {
-  fprintf(f, "<pre><code>\n");
+  fprintf(f, "<pre><code>");
 }
 
 void html_code_end(FILE* f) {
@@ -371,7 +348,63 @@ char* p_getline(FILE* in) {
   return input;
 }
 
-void addLineToPage(line l, page* p) {
+int compare_dates(date* d1, date* d2) {
+  /* 1 means d1 is greater */
+  if (d1->year > d2->year) {
+    return 1;
+  } else if (d1->year < d2->year) {
+    return -1;
+  } else {
+    if (d1->month > d2->month) {
+      return 1;
+    } else if (d1->month < d2->month) {
+      return -1;
+    } else {
+      if (d1->day > d2->day) {
+        return 1;
+      } else if (d1->day < d2->day) {
+        return -1;
+      } else {
+        return 0;
+      }
+    }
+  }
+}
+
+int compare_page_dates(const void* a, const void* b) {
+  const page* p1 = a;
+  const page* p2 = b;
+  date p1_date = *(p1->created);
+  date p2_date = *(p2->created);
+  if (p1->updated != NULL)
+    p1_date = *(p1->updated);
+  if (p2->updated != NULL)
+    p2_date = *(p2->updated);
+
+  return compare_dates(&p2_date, &p1_date);
+}
+
+void parse_date(char* text, page* p) {
+  /* / yyyy-mm-dd, yyyy-mm-dd */
+  size_t len = strlen(text);
+  date* created = malloc(sizeof(date)); 
+  date* updated = malloc(sizeof(date));
+  *created = (date) { 0, 0, 0 };
+  *updated = (date) { 0, 0, 0 };
+  if (len == 10) {
+    /* single date */
+    sscanf(text, "%d-%d-%d", &(created->year), &(created->month), &(created->day));
+    p->created = created;
+  } else if (len == 22) {
+    sscanf(text, "%d-%d-%d, %d-%d-%d", &(created->year), &(created->month), &(created->day), &(updated->year), &(updated->month), &(updated->day));
+    p->updated = updated;
+    p->created = created;
+  } else {
+    printf("Invalid date (/) found in %s: \n\t%s\n", p->title, text);
+  }
+}
+
+void add_line_to_page(line l, page* p) {
   p->numlines++;
   p->lines = realloc(p->lines, sizeof(line) * p->numlines);
   p->lines[p->numlines - 1] = l;
@@ -401,7 +434,7 @@ int generate_page(FILE* out, page* p) {
       mode = OFF;
     }
 
-    if (mode == UNORDERED && l.type != PI_ORDERED) {
+    if (mode == UNORDERED && l.type != PI_UNORDERED) {
       html_unordered_list_end(out);
       mode = OFF;
     }
@@ -484,9 +517,14 @@ void generate_index(page* pages, int page_count) {
   FILE* out = fopen("../index.html", "w");
   html_header(out, "Park Imminent");
   fprintf(out, "<header><h1>Park</br>Imminent</br></h1></header>");
-  fprintf(out, "<ul>");
+  fprintf(out, "<ul class=\"index_by_date\">");
   for (int i = 0; i < page_count; i++) {
-    fprintf(out, "<li><a href=\"./site/%s.html\">%s</a></li>\n", pages[i].filename, pages[i].title);
+    if (pages[i].updated == NULL) {
+      fprintf(out, "<li><a href=\"./site/%s.html\"><h2>%s</h2></a><time>%d-%02d-%02d</time></li>\n", pages[i].filename, pages[i].title, pages[i].created->year, pages[i].created->month, pages[i].created->day);
+    } else {
+      /* TODO only show updated text if the update was in the last month? */
+      fprintf(out, "<li><a href=\"./site/%s.html\"><h2>%s (updated)</h2></a><time>%d-%02d-%02d</time></li>\n", pages[i].filename, pages[i].title, pages[i].updated->year, pages[i].updated->month, pages[i].updated->day);
+    }
   }
   fprintf(out, "</ul>");
   html_footer(out);
@@ -518,9 +556,11 @@ void parse_file(FILE* file, page** p_pages, int* page_count) {
       page* p = *p_pages;
       int address = *page_count - 1;
       p[address] = (page) { .title = rawline, .section = section, .filename = fn, .parent = NULL, .lines = NULL, .numlines = 0 };
+    } else if (rune == PI_DATE) {
+      parse_date(text, *p_pages + *page_count - 1); 
     } else {
       line l = { .type = rune, .text = text };
-      addLineToPage(l, *p_pages + *page_count - 1);
+      add_line_to_page(l, *p_pages + *page_count - 1);
     }
   }
 }
@@ -554,6 +594,9 @@ int main() {
     }
   }
   /* close the FILE */
+
+  qsort(raw_pages, num_pages, sizeof(page), compare_page_dates); 
+
   generate_pages(raw_pages, num_pages);
   generate_index(raw_pages, num_pages);
 
